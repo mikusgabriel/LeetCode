4188a79d99bc017d7393dcc6de82b8aa344ae6e7
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2024-12-02 11:44:38 -0500

    2024-12-02

--- advent_of_code/12-02/main.py
+++ advent_of_code/12-02/main.py
@@ -1,37 +1,62 @@
-def part_two(input_str: str) -> int:
-    def report_is_safe(report):
-        diffs = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+def isValid(line):
+    numbers = [int(x) for x in line.split()]
+    if len(line) == 0:
+        return False
+    
+    descending = False
+    difference = 0
+    for i in range(len(numbers)-1):
+        difference += numbers[i] - numbers[i+1]
 
-        increase = sum(1 if d > 0 else -1 for d in diffs) > 0
+    if difference > 0:
+        descending = True
 
-        follows_trend = all(d > 0 if increase else d < 0 for d in diffs)
+    for i in range (len(numbers)-1):
+        if descending is True:
+            if numbers[i] - numbers[i+1] > 0 and  numbers[i] - numbers[i+1] < 4:
+                pass                    
 
-        differs_safely = all(1 <= abs(d) <= 3 for d in diffs)
+            else:
+                return False
+        elif descending is False:
+            if numbers[i] - numbers[i+1] < 0 and numbers[i] - numbers[i+1] > -4:
+                pass
+            else:
+                return False
+            
+    return True
 
-        return follows_trend and differs_safely
 
-    count = 0
+        
 
-    for line in input_str.splitlines():
-        report = [int(x) for x in line.split()]
+def partTwo(line):
+    new_array = []
+    for i in range(len(line)):
+        new_array = line[:i] + line[i+2:]
+        if isValid(new_array):
+            return True
 
-        if len(report) < 2:
-            continue
+    return False  
 
-        if report_is_safe(report):
-            count += 1
-            continue
 
-        for i in range(len(report)):
-            sliced_report = report[:i] + report[i + 1:]
-            if report_is_safe(sliced_report):
-                count += 1
-                break  
 
-    return count
+
+
+
+
+
+    
+
+
+
 
 f = open("data.txt", "r")
-arr = f.read()
+arr = f.read().split("\n")
 
-reponse = part_two(arr)
-print(reponse)
+summ = 0
+for line in arr:
+    if isValid(line):
+        summ+=1
+    elif partTwo(line):
+        summ+=1
+print(summ)
