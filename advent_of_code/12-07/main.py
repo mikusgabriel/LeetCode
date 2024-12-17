f2452d8896407f27c24e2f4e546781b4f677d3ac
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2024-12-17 11:53:39 -0500

    2024-12-07

--- advent_of_code/12-07/main.py
+++ advent_of_code/12-07/main.py
@@ -9,7 +9,7 @@ pattern = r"(\d+)"
 
 
 def test(goal, equation):
-
+    
     for i in product(["*", "+","||"], repeat=len(equation) - 1):
         compare_goal = 0
         j = 0
@@ -19,39 +19,37 @@ def test(goal, equation):
             if j == 0:
                 if i[equation_sign_cmp] == "||":
                     compare_goal = int(str(equation[j]) + str(equation[j + 1]))
-                
-                else:
-                    compare_goal = eval(
-                    str(equation[j]) + i[equation_sign_cmp] + str(equation[j + 1])
-                    )
+                elif i[equation_sign_cmp] == "+":
+                    compare_goal = equation[j] + equation[j + 1] 
+                elif i[equation_sign_cmp] == "*":
+                    compare_goal = equation[j] * equation[j + 1]
                 j += 1
 
             else:
                 if i[equation_sign_cmp] == "||":
                     compare_goal = int(str(compare_goal) + str(equation[j]))
-                else:
-                    compare_goal = eval(
-                        str(compare_goal) + i[equation_sign_cmp] + str(equation[j])
-                    )
+                elif i[equation_sign_cmp] == "+":
+                    compare_goal += equation[j]
+                elif i[equation_sign_cmp] == "*":
+                    compare_goal *= equation[j]
+
+                  
             j+=1
             equation_sign_cmp+=1
    
         if compare_goal == goal:
 
-            print(equation,True)
             return goal
         
     return 0
 
 summ = 0
-cmp = 0
 for line in line_input:
     result = re.findall(pattern, line)
-    print(cmp)
+   
 
-    equation = result[1:]
+    equation = list(map(int,result[1:]))
     goal = int(result[0])
     summ += test(goal, equation)
-    cmp +=1
 
 print(summ)
